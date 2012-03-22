8f47f01ad9d441832e3ccadf02115e237390662c
Casey Link <unnamedrambler@gmail.com>
2012-03-22 12:29:48 -0500

    Android Test project skeleton that depends on iocipher

---
+++ src/info/guardianproject/test/iocipher/SimpleReadTest.java
@@ -0,0 +1,16 @@
+package info.guardianproject.test.iocipher;
+
+import android.test.AndroidTestCase;
+
+public class SimpleReadTest extends AndroidTestCase {
+
+	protected void setUp() {
+		
+	}
+	protected void tearDown() {
+		
+	}
+	public void testRead() {
+		assertTrue(true);
+	}
+}
