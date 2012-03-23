92427d741c784e65310a55f31c1b277ba3ee5cac
Hans-Christoph Steiner <hans@eds.org>
2012-03-23 19:53:29 -0400

    more directory tests, including working mkdir() test

--- src/info/guardianproject/test/iocipher/FileTest.java
+++ src/info/guardianproject/test/iocipher/FileTest.java
@@ -1,6 +1,7 @@
 package info.guardianproject.test.iocipher;
 
 import info.guardianproject.iocipher.File;
+import android.content.Context;
 import android.test.AndroidTestCase;
 import android.util.Log;
 
@@ -8,9 +9,11 @@ public class FileTest extends AndroidTestCase {
 	private final static String TAG = "AndroidTestCase";
 	
 	private java.io.File app_tmp;
+	private String app_tmp_dir;
 
 	protected void setUp() {
-		
+		app_tmp = mContext.getDir("tmp", Context.MODE_PRIVATE).getAbsoluteFile();
+		app_tmp_dir = app_tmp.getAbsolutePath();
 	}
 	protected void tearDown() {
 		
@@ -19,6 +22,18 @@ public class FileTest extends AndroidTestCase {
 		File f = new File("");
 		try {
 			assertFalse(f.exists());
+		} catch (ExceptionInInitializerError e) {
+			Log.e(TAG, e.getCause().toString());
+			assertFalse(true);
+		}
+	}
+	public void testSlashList() {
+		File f = new File("/");
+		try {
+			for (String file : f.list()) {
+				Log.i(TAG, "file: " + file);
+			}
+			assertTrue(true);
 		} catch (ExceptionInInitializerError e) {
 			Log.e(TAG, e.getCause().toString());
 			assertFalse(true);
@@ -51,9 +66,36 @@ public class FileTest extends AndroidTestCase {
 			assertFalse(true);
 		}
 	}
-	public void testCreateDirInTmp() {
-		//File f = new File("/tmp/test.iocipher.dir." + Integer.toString((int)(Math.random() * 1024)));
-		File f = new File("/TEST");
+	public void testCreateDirInRootNoPerms() {
+		File f = new File("/NonExistant");
+		try {
+			assertFalse(f.mkdir());
+		} catch (ExceptionInInitializerError e) {
+			Log.e(TAG, e.getCause().toString());
+			assertFalse(true);
+		}
+	}
+	
+	public void testDataLocalExists() {
+		File f = new File("/data/local");
+		try {
+			assertTrue(f.exists());
+		} catch (ExceptionInInitializerError e) {
+			Log.e(TAG, e.getCause().toString());
+			assertFalse(true);
+		}
+	}
+	public void testDataLocalIsDirectory() {
+		File f = new File("/data/local");
+		try {
+			assertTrue(f.isDirectory());
+		} catch (ExceptionInInitializerError e) {
+			Log.e(TAG, e.getCause().toString());
+			assertFalse(true);
+		}
+	}
+	public void testAppTmpMkdir() {
+		File f = new File(app_tmp_dir + "/test.iocipher.dir." + Integer.toString((int)(Math.random() * 1024)));
 		try {
 			assertTrue(f.mkdir());
 		} catch (ExceptionInInitializerError e) {
