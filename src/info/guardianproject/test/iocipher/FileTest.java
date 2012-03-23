78b66b0a7836dcedac53df08a807b5d518451efc
Hans-Christoph Steiner <hans@eds.org>
2012-03-23 19:33:48 -0400

    added some very basic tests

--- src/info/guardianproject/test/iocipher/FileTest.java
+++ src/info/guardianproject/test/iocipher/FileTest.java
@@ -1,8 +1,8 @@
 package info.guardianproject.test.iocipher;
 
+import info.guardianproject.iocipher.File;
 import android.test.AndroidTestCase;
 import android.util.Log;
-import info.guardianproject.iocipher.File;
 
 public class FileTest extends AndroidTestCase {
 
@@ -16,6 +16,43 @@ public class FileTest extends AndroidTestCase {
 		File f = new File("");
 		try {
 			assertFalse(f.exists());
+		} catch (ExceptionInInitializerError e) {
+			Log.e("IOCipherTests", e.getCause().toString());
+			assertFalse(true);
+		}
+	}
+	public void testSlashIsDirectory() {
+		File f = new File("/");
+		try {
+			assertTrue(f.isDirectory());
+		} catch (ExceptionInInitializerError e) {
+			Log.e("IOCipherTests", e.getCause().toString());
+			assertFalse(true);
+		}
+	}
+	public void testSlashIsFile() {
+		File f = new File("/");
+		try {
+			assertFalse(f.isFile());
+		} catch (ExceptionInInitializerError e) {
+			Log.e("IOCipherTests", e.getCause().toString());
+			assertFalse(true);
+		}
+	}
+	public void testSlashIsAbsolute() {
+		File f = new File("/");
+		try {
+			assertTrue(f.isAbsolute());
+		} catch (ExceptionInInitializerError e) {
+			Log.e("IOCipherTests", e.getCause().toString());
+			assertFalse(true);
+		}
+	}
+	public void testCreateDirInTmp() {
+		//File f = new File("/tmp/test.iocipher.dir." + Integer.toString((int)(Math.random() * 1024)));
+		File f = new File("/TEST");
+		try {
+			assertTrue(f.mkdir());
 		} catch (ExceptionInInitializerError e) {
 			Log.e("IOCipherTests", e.getCause().toString());
 			assertFalse(true);
